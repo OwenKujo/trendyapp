d6f94ddd0ae46ced2cbf6cd68001188d0e356a1f
Owen Kujo <rapeepat.cadflow@gmail.com>
2024-10-04 00:43:46 +0700

    Update PinCard.jsx

--- frontend/src/components/PinCard.jsx
+++ frontend/src/components/PinCard.jsx
@@ -8,7 +8,7 @@ const PinCard = ({ pin }) => {
         <img
           src={pin.image.url}
           alt=""
-          className="w-full h-full object-cover transition-transform duration-300 transform group-hover:scale-105" // Image scaling effect on hover
+          className="w-full h-full object-cover transition-transform duration-300 transform group-hover:scale-55" // Image scaling effect on hover
         />
         <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-50 transition-opacity duration-300 flex items-center justify-center">
           <div className="flex flex-col justify-center items-center gap-2">
