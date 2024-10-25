e4d832cdb25bf2bb8d25269e287539f8fd6842ae
Owen Kujo <rapeepat.cadflow@gmail.com>
2024-10-25 17:34:23 +0700

    un shuffle pins

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,18 +6,6 @@ import PinCard from "../components/PinCard";
 const Home = () => {
   const { pins, loading } = PinData();
 
-  // Function to shuffle an array
-  const shuffleArray = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  };
-
-  // Shuffle pins if they exist
-  const shuffledPins = pins ? shuffleArray([...pins]) : [];
-
   return (
     <div>
       {loading ? (
@@ -27,8 +15,8 @@ const Home = () => {
           <div className="px-4 py-6 sm:px-0">
             {/* Updated gap for less spacing */}
             <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-2">
-              {shuffledPins.length > 0 ? (
-                shuffledPins.map((e) => (
+              {pins.length > 0 ? (
+                pins.map((e) => (
                   <div className="flex justify-center" key={e._id}>
                     <PinCard pin={e} />
                   </div>
